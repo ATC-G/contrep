14da1f9b457f972dcce119725a017e024b3adbd3
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2024-02-02 00:02:19 -0500

    adding forma/metodo pago al pagar

--- src/components/Cobranza/Pagar.js
+++ src/components/Cobranza/Pagar.js
@@ -1,27 +1,117 @@
-import { Button } from "reactstrap"
+import { useFormik } from "formik";
+import { Button, Col, Form, Label, Row } from "reactstrap";
+import * as Yup from "yup";
+import { FIELD_REQUIRED, SELECT_OPTION } from "../../constants/messages";
+import Select from "react-select";
+import { formaPagoOpt, metodoPagoOpt } from "../../constants/utils";
 
-const Pagar = ({onHandlePayment, setOpen, row, idx}) => {
+const Pagar = ({ onHandlePayment, setOpen, row, idx }) => {
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      formaPago: "",
+    },
+    validationSchema: Yup.object({
+      formaPago: Yup.string().required(FIELD_REQUIRED),
+      metodoPago: Yup.string().required(FIELD_REQUIRED),
+    }),
+    onSubmit: async (values) => {
+      console.log(values);
+      //validaciones antes de enviarlo
+      onHandlePayment(row, idx, values.formaPago, values.metodoPago);
+    },
+  });
 
-    return (
-        <>
-            <h3>Seguro que deseas pagar la referencia?</h3>
-            <hr />
-            <div className="d-flex">
-                <div className="pe-2">
-                <Button color="primary" type="button" className="me-2" onClick={()=>onHandlePayment(row, idx)}>
-                    Aceptar
-                </Button>
-                </div>
-                <div>
-                <Button color="light" type="button" className="me-2" onClick={() =>setOpen(false)}>
-                    Cancelar
-                </Button>
-                </div>
-            </div>
-            
-        </>
-    )
-    
-}
+  return (
+    <>
+      <Form
+        className="needs-validation"
+        id="tooltipForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit();
+          return false;
+        }}
+      >
+        <h4>Seguro que deseas pagar la referencia?</h4>
+        <Row>
+          <Col xs="12" md="12" className="mb-3">
+            <Label className="mb-0">Forma de pago</Label>
+            <Select
+              classNamePrefix="select2-selection"
+              placeholder={SELECT_OPTION}
+              options={formaPagoOpt}
+              value={
+                formik.values.formaPago
+                  ? {
+                      value: formik.values.formaPago,
+                      label:
+                        formaPagoOpt.find(
+                          (it) => it.value === formik.values.formaPago
+                        )?.label ?? "",
+                    }
+                  : null
+              }
+              onChange={(value) => {
+                formik.setFieldValue("formaPago", value.value);
+              }}
+            />
+            {formik.errors.formaPago && (
+              <div className="invalid-tooltip d-block">
+                {formik.errors.formaPago}
+              </div>
+            )}
+          </Col>
+          <Col xs="12" md="12">
+            <Label className="mb-0">Método de pago</Label>
+            <Select
+              classNamePrefix="select2-selection"
+              placeholder={SELECT_OPTION}
+              options={metodoPagoOpt}
+              value={
+                formik.values.metodoPago
+                  ? {
+                      value: formik.values.metodoPago,
+                      label:
+                        metodoPagoOpt.find(
+                          (it) => it.value === formik.values.metodoPago
+                        )?.label ?? "",
+                    }
+                  : null
+              }
+              onChange={(value) => {
+                formik.setFieldValue("metodoPago", value.value);
+              }}
+            />
 
-export default Pagar
+            {formik.errors.metodoPago && (
+              <div className="invalid-tooltip d-block">
+                {formik.errors.metodoPago}
+              </div>
+            )}
+          </Col>
+        </Row>
+        <hr />
+        <div className="d-flex">
+          <div className="pe-2">
+            <Button color="primary" type="submit" className="me-2">
+              Aceptar
+            </Button>
+          </div>
+          <div>
+            <Button
+              color="light"
+              type="button"
+              className="me-2"
+              onClick={() => setOpen(false)}
+            >
+              Cancelar
+            </Button>
+          </div>
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default Pagar;
--- src/constants/utils.js
+++ src/constants/utils.js
@@ -1,50 +1,80 @@
 export const mesesOpt = [
-    {
-        value: 0,
-        label: 'Enero'
-    },
-    {
-        value: 1,
-        label: 'Febrero'
-    },
-    {
-        value: 2,
-        label: 'Marzo'
-    },
-    {
-        value: 3,
-        label: 'Abril'
-    },
-    {
-        value: 4,
-        label: 'Mayo'
-    },
-    {
-        value: 5,
-        label: 'Junio'
-    },
-    {
-        value: 6,
-        label: 'Julio'
-    },
-    {
-        value: 7,
-        label: 'Agosto'
-    },
-    {
-        value: 8,
-        label: 'Septiembre'
-    },
-    {
-        value: 9,
-        label: 'Octubre'
-    },
-    {
-        value: 10,
-        label: 'Noviembre'
-    },
-    {
-        value: 11,
-        label: 'Diciembre'
-    }
-]
+  {
+    value: 0,
+    label: "Enero",
+  },
+  {
+    value: 1,
+    label: "Febrero",
+  },
+  {
+    value: 2,
+    label: "Marzo",
+  },
+  {
+    value: 3,
+    label: "Abril",
+  },
+  {
+    value: 4,
+    label: "Mayo",
+  },
+  {
+    value: 5,
+    label: "Junio",
+  },
+  {
+    value: 6,
+    label: "Julio",
+  },
+  {
+    value: 7,
+    label: "Agosto",
+  },
+  {
+    value: 8,
+    label: "Septiembre",
+  },
+  {
+    value: 9,
+    label: "Octubre",
+  },
+  {
+    value: 10,
+    label: "Noviembre",
+  },
+  {
+    value: 11,
+    label: "Diciembre",
+  },
+];
+
+export const formaPagoOpt = [
+  { value: "01", label: "Efectivo" },
+  { value: "02", label: "Cheque nominativo" },
+  { value: "03", label: "Transferencia electrónica de fondos" },
+  { value: "04", label: "Tarjeta de crédito" },
+  { value: "05", label: "Monedero electrónico" },
+  { value: "06", label: "Dinero electrónico" },
+  { value: "08", label: "Vales de despensa" },
+  { value: "12", label: "Dación en pago" },
+  { value: "13", label: "Pago por subrogación" },
+  { value: "14", label: "Pago por consignación" },
+  { value: "15", label: "Condonación" },
+  { value: "17", label: "Compensación" },
+  { value: "23", label: "Novación" },
+  { value: "24", label: "Confusión" },
+  { value: "25", label: "Remisión de deuda" },
+  { value: "26", label: "Prescripción o caducidad" },
+  { value: "27", label: "A satisfacción del acreedor" },
+  { value: "28", label: "Tarjeta de débito" },
+  { value: "29", label: "Tarjeta de servicios" },
+  { value: "30", label: "Aplicación de anticipos" },
+  { value: "31", label: "Intermediario pagos" },
+  { value: "99", label: "Por definir" },
+];
+
+export const metodoPagoOpt = [
+  { value: "PUE", label: "Pago en una sola exhibición" },
+  { value: "PPD", label: "Pago en parcialidades o diferido" },
+];
